53daea56a32a9f5b79c811c062d0d4e8f11f7dee
Viktor Tsvigo <11_70@bk.ru>
2024-05-05 05:22:51 +0300

    Описание коммита

--- dialog.cpp
+++ dialog.cpp
@@ -20,8 +20,33 @@ Dialog::Dialog(QWidget *parent)
      {
      timer=new QTimer; 
 connect(timer,SIGNAL(timeout()),this,SLOT(do_timer())); 
-timer->start(60000); // 50 миллисекунд... 3 часа=10800000 мс; 5 минут = 300000 мс;  60000 мс = минута
-     
+timer->start(10800000); // 50 миллисекунд... 3 часа=10800000 мс; 5 минут = 300000 мс;  60000 мс = минута
+//########################################################################################################
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+   //     QProcess::execute("/home/viktor/my_scripts_4/imena_katalogov.sh");
+QProcess process;
+process.start("bash", QStringList() << "/home/viktor/my_scripts_4/imena_katalogov.sh");
+process.waitForFinished();
+QString output = process.readAllStandardOutput();
+std::cout << output.toStdString() << std::endl;
+//########################################################################################################
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+   //     QProcess::execute("/home/viktor/my_scripts_4/imena_katalogov.sh");
+QProcess process2;
+process2.start("bash", QStringList() << "/home/viktor/my_scripts_4/podkluchenie_k_git.sh");
+process2.waitForFinished();
+QString output2 = process2.readAllStandardOutput();
+std::cout << output2.toStdString() << std::endl;
+//########################################################################################################
+QProcess process3;
+process3.start("bash", QStringList() << "/home/viktor/my_scripts_4/auto_git.sh");
+process3.waitForFinished(-1);
+QString output3 = process3.readAllStandardOutput();
+std::cout << output3.toStdString() << std::endl;
+
+int exitCode = process3.exitCode();    
+qDebug() << "exitCode = " << exitCode;//. [1](https://intuit.ru/studies/courses/3479/721/lecture/25564?page=3)
+//########################################################################################################     
      }
 
 Dialog::~Dialog()
